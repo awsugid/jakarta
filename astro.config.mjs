a758c2b993cba75d9a6111ae2ba5a7c5f0eca44b
Avei20 <muhammadabdulazizalghofari@gmail.com>
2026-05-11 14:19:34 +0700

    feat: Adding SiteMap for indexing Google Search

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,13 @@ import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
+import sitemap from "@astrojs/sitemap";
 
 import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), mdx()],
+  site: process.env.SITE_URL || "https://jakarta.awscommunity.id",
+  integrations: [react(), mdx(), sitemap()],
 
   vite: {
     plugins: [tailwindcss()],
